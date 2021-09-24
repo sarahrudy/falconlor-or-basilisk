ef650b507f84cb074c777b03f51daa3c08d43ec5
Sarah <sarahrudy@gmail.com>
2021-09-23 18:35:17 -0600

    Add contents to NavBar component

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,2 +1,16 @@
 import React from 'react' 
+import turingSortingHat from '../../Images/turing_sorting_hat.png'
+import '../NavBar/NavBar.css'
 
+const NavBar = () => {
+
+  return (
+    <nav>
+      <Link to='/'>
+        <img src={ turingSortingHat } className="turing-sorting-hat" alt="turing logo with sorting hat"></img>
+      </Link>
+    </nav>
+  )
+}
+
+export default NavBar
