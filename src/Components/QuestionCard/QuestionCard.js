e886beaa5ad141c0b8cf5ed8788ec46165e26a91
Sarah <sarahrudy@gmail.com>
2021-09-24 15:54:46 -0600

    Get images to render on page

--- src/Components/QuestionCard/QuestionCard.js
+++ src/Components/QuestionCard/QuestionCard.js
@@ -1,14 +1,15 @@
-import { questionsMockData } from '../../data/questionsMockData'
 import './QuestionCard.css'
 
-const QuestionCard = ({ questionProp }) => {
-  console.log(questionProp)
+const QuestionCard = ({ questionProp, characterProp }) => {
+  // console.log(questionProp)
+  // console.log(characterProp)
   return (
     <article>
       <div className="card-container">
         <p>Question</p>
         <p>Answer 1</p>
         <p>Answer 2</p>
+        <img src={characterProp.image}></img>
       </div>
     </article>
   )
--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { questionsMockData } from '../../data/questionsMockData'
 import '../Questions/Questions.css'
-// import { fetchImages } from '../../apiCalls'
+import { fetchImages } from '../../apiCalls'
 import QuestionCard from '../QuestionCard/QuestionCard'
 
 const Questions = () => {
   const [questions, setQuestions] = useState([])
-  // const [images, setImages] = useState([])
+  const [images, setImages] = useState([])
 
   useEffect(() => {
-    // fetchImages()
-    // .then(data => setImages(data))
-    // .then(data => console.log(data))
+    fetchImages()
+    .then(data => setImages(data))
 
     setQuestions(questionsMockData)
   }, [])
-
-  const singleQuestion = questions.map(question => {
-    return (
-      <QuestionCard questionProp= { question } />
+    
+    const singleQuestion = questions.map((question, i) => { 
+      return (
+        <QuestionCard 
+        questionProp= { question } 
+        characterProp= { images && images[i] }
+      />
     )
   })
 
