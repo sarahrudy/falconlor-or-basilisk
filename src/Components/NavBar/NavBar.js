fa868cd8b1da181f2ffbfaf98b8526f0c6d00e52
Sarah <sarahrudy@gmail.com>
2021-09-23 20:16:49 -0600

    Import Link from react-router-dom

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import turingSortingHat from '../../Images/turing_sorting_hat.png'
+import { Link } from 'react-router-dom'
 import '../NavBar/NavBar.css'
 
 const NavBar = () => {
@@ -9,6 +10,7 @@ const NavBar = () => {
       <Link to='/'>
         <img src={ turingSortingHat } className="turing-sorting-hat" alt="turing logo with sorting hat"></img>
       </Link>
+      <h1>  Turing Sorting Hat</h1>
     </nav>
   )
 }
