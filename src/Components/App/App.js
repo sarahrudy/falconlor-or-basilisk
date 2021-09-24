f9453ef3b8affb4e83df1dbb105736e7277826d1
Sarah <sarahrudy@gmail.com>
2021-09-24 15:11:00 -0600

    Import Questions component to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import '../App/App.css'
 import NavBar from '../NavBar/NavBar'
 import { Route } from 'react-router-dom'
+import Questions from '../Questions/Questions'
 
 
 const App = () => {
 
   return (
-    <div className="App">
-      <Route exact path="/" component={ NavBar } />
-    </div>
+    <main className="App">
+      <NavBar />
+      <Route exact path="/" component={ Questions } />
+    </main>
   )
 }
 
