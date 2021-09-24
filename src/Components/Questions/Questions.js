744e48e373b7288f811d0b56b1444bab6d1b103e
Sarah <sarahrudy@gmail.com>
2021-09-24 15:13:01 -0600

    Import QuestionCard to Questions component

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import questions from '../../data/questions'
+import { questionsMockData } from '../../data/questionsMockData'
 import '../Questions/Questions.css'
-import { fetchImages } from '../../apiCalls'
+// import { fetchImages } from '../../apiCalls'
+import QuestionCard from '../QuestionCard/QuestionCard'
 
-const Questions = (props) => {
+const Questions = () => {
   const [questions, setQuestions] = useState([])
+  // const [images, setImages] = useState([])
 
   useEffect(() => {
-    fetchImages()
-    // .then(data => setQuestions(data))
-    .then(data => console.log(data))
+    // fetchImages()
+    // .then(data => setImages(data))
+    // .then(data => console.log(data))
+
+    setQuestions(questionsMockData)
   }, [])
+
+  const singleQuestion = questions.map(question => {
+    return (
+      <QuestionCard questionProp= { question } />
+    )
+  })
 
   return (
     <div className="questions-container">
-      { questions }
+      { singleQuestion }
     </div>
   )
 }
