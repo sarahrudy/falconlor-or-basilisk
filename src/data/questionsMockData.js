731b367042d138de7e6c7d1890be7f1b5d14120b
Sarah <sarahrudy@gmail.com>
2021-09-25 14:25:01 -0600

    Add more questions to quiz

--- src/data/questionsMockData.js
+++ src/data/questionsMockData.js
@@ -1,12 +1,77 @@
 export const questionsMockData = [
   {
-   question: 'Which program are you drawn towards?', 
-   answer_be: 'BE answer',
-   answer_fe: 'FE answer',
+    question: 'When building IKEA furniture, I prefer to build the item',
+    answer_be: 'without looking at the instructions on how to build it',
+    answer_fe: 'while looking at the pictures and following step by step',
   },
   {
-   question: 'Are you more of a visual or conceptual person?',
-   answer_be: 'conceptual',
-   answer_fe: 'visual',
-  }
+    question: 'If I worked at Chipotle, I would prefer to work:',
+    answer_fe: 'in the front of the house building delicious burritos for customers',
+    answer_be: 'in the back of the house making sure everything is timed correctly so the front of the house never runs out of food to serve',
+  },
+  {
+    question: 'If I do a little more research on CSS and what APIs are, I would rather:',
+    answer_be: 'fix a complex CSS file by going through each property',
+    answer_fe: 'reformat an API so that frontend is getting data in the correct format',
+  },
+  {
+    question: 'I would prefer to:',
+    answer_fe: 'figure out the logic on how to display data',
+    answer_be: 'figure out the logic on how to manipulate data',
+  },
+  {
+    question: 'Pick one:',
+    answer_be: 'I like to build things from scratch without needing to see them finished',
+    answer_fe: 'I like to see things I build come to fruition ',
+  },
+  {
+    question: 'If someone tells me directions:',
+    answer_fe: 'I understand it better if they show me on a map',
+    answer_be: 'I can picture exactly what they’re talking about without needing to see it',
+  },
+  {
+    question: 'When thinking about how a user will interact with my app:',
+    answer_be: 'I am not super concerned about how the user will interact with my app',
+    answer_fe: 'I am interested in making the app user friendly and accessible by everyone',
+  },
+  {
+    question: 'I would rather:',
+    answer_be: 'build something from scratch',
+    answer_fe: 'build on top of an existing boilerplate',
+  },
+  {
+    question: 'I prefer to:',
+    answer_fe: 'reformat an API so that frontend is getting data in the correct format',
+    answer_be: 'fix a complex CSS file by going through each property',
+  },
+  {
+    question: 'When I receive directions:',
+    answer_be: 'I can easily conceptualize what to do next',
+    answer_fe: 'I would prefer to write things down first',
+  },
+  {
+    question: 'I would rather:',
+    answer_fe: 'paint a picture',
+    answer_be: 'build the canvas',
+  },
+  {
+    question: 'I would rather:',
+    answer_be: 'build a game of Battleship from scratch with Ruby',
+    answer_fe: 'write the logic for when a user clicks a button with JavaScript ',
+  },
+  {
+    question: 'When building an app:',
+    answer_fe: 'I am more interested in how my app will interact with the server',
+    answer_be: 'I am more interested in how a user will interact with my app',
+  },
+  {
+    question: 'As I research my future career as a software developer, I am more interested in studying:',
+    answer_fe: 'JavaScript / React',
+    answer_be: 'Ruby / Ruby on Rails',
+  },
+  {
+    question: 'Without listening to the advice of anyone else, which program are you more drawn towards?',
+    answer_be: 'BE',
+    answer_fe: 'FE',
+  },
 ]
