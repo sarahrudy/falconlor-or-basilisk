443e86c67dfcf88cd770ff71cd061f49aa2b51c0
Sarah <sarahrudy@gmail.com>
2021-09-26 16:03:19 -0600

    Add more questions

--- src/data/questionsMockData.js
+++ src/data/questionsMockData.js
@@ -1,10 +1,10 @@
 export const questionsMockData = [
   {
     id: 1,
-    question: 'When building IKEA furniture, I prefer to build the item:',
+    question: 'The following sounds more interesting to me:',
     answers: [
-      {answer: 'without looking at the instructions on how to build it', program: 'BE'},
-      {answer: 'while looking at the pictures and following step by step', program: 'FE'},
+      {answer: 'Analyze client-side performance', program: 'FE'},
+      {answer: 'Database management', program: 'BE'},
     ]
   },
   {
@@ -27,96 +27,176 @@ export const questionsMockData = [
     id: 4,
     question: 'Pick one:',
     answers: [
-      { answer: 'I like to build things from scratch without needing to see them finished', program: 'BE' },
-      { answer: 'When I build things I like to see things I build come to fruition', program: 'FE' },
+      { answer: 'I like to build things from scratch without seeing the end result', program: 'BE' },
+      { answer: 'When I build things I like to see them finished', program: 'FE' },
     ]
   },
-  {
-    id: 5,
-    question: 'If someone tells me directions:',
-    answers: [
-      {answer: 'I will understand quickly if I can see it on a map', program: 'FE'},
-      {answer: 'I can picture exactly what they’re talking about without needing to see it', program: 'BE'},
-    ]
-  },
-  {
-    id: 6,
-    question: 'When thinking about how a user will interact with my app:',
-    answers: [
-      {answer: 'I am not super concerned about how the user will interact with my app', program: 'BE'},
-      {answer: 'I am interested in making the app user friendly and accessible by everyone', program: 'FE'},
-    ]
-  },
-  {
-    id: 7,
-    question: 'I would rather:',
-    answers: [
-      {answer: 'build something from scratch', program: 'BE'},
-      {answer: 'build on top of an existing boilerplate', program: 'FE'},
-    ]
-  },
-  {
-    id: 8,
-    question: 'When I receive directions:',
-    answers: [
-      {answer: 'I can easily conceptualize what to do next', program: 'BE'},
-      {answer: 'I would prefer to write things down first', program: 'FE'},
-    ]
-  },
-  {
-    id: 9,
-    question: 'I would rather:',
-    answers: [
-      {answer: 'paint a picture of the mountains', program: 'FE'},
-      {answer: 'build the canvas the picture is painted on', program: 'BE'},
-    ]
-  },
-  {
-    id: 10,
-    question: 'Making an application visually appealing is important to me',
-    answers: [
-      {answer: 'false', program: 'BE'},
-      {answer: 'true', program: 'FE'},
-    ]
-  },
-  {
-    id: 11,
-    question: 'When building an app:',
-    answers: [
-      {answer: 'I am more interested in how a user will interact with my app', program: 'FE'},
-      {answer: 'I am more interested in how my app will interact with the server', program: 'BE'},
-    ]
-  },
-  {
-    id: 12,
-    question: 'I would rather:',
-    answers: [
-      {answer: 'write the logic for when a user clicks a button with JavaScript', program: 'FE'},
-      {answer: 'build a game of Battleship from scratch with Ruby', program: 'BE'},
-    ]
-  },
-  {
-    id: 13,
-    question: 'If I do a little more research on CSS and what APIs are, I would rather:',
-    answers: [
-      {answer: 'reformat a server-side request so that frontend is getting data in the correct format', program: 'BE'},
-      {answer: 'fix a complex CSS file by going through each property one by one', program: 'FE'},
-    ]
-  },
-  {
-    id: 14,
-    question: 'As I research my future career as a software developer, I am more interested in studying these tech stack:',
-    answers: [
-      {answer: 'JavaScript / React / HTML / CSS', program: 'FE'},
-      {answer: 'Ruby / Ruby on Rails / Express / PostgreSQL', program: 'BE'},
-    ]
-  },
-  {
-    id: 15,
-    question: 'Without listening to the advice of anyone else, my gut is telling me I\'d be a better fit for:',
-    answers: [
-      {answer: 'The Frontend program', program: 'FE'},
-      {answer: 'the Backend program', program: 'BE'},
-    ]
-  },
+  // {
+  //   id: 5,
+  //   question: 'If someone tells me directions:',
+  //   answers: [
+  //     {answer: 'I will get my bearings more quickly if I can see it on a map', program: 'FE'},
+  //     {answer: 'I can picture exactly what they’re talking about without needing to see it', program: 'BE'},
+  //   ]
+  // },
+  // {
+  //   id: 6,
+  //   question: 'When thinking about how a user will interact with my app:',
+  //   answers: [
+  //     {answer: 'I am not super concerned about how the user will interact with my app', program: 'BE'},
+  //     {answer: 'I am interested in making the app user friendly and accessible by everyone', program: 'FE'},
+  //   ]
+  // },
+  // {
+  //   id: 7,
+  //   question: 'I would rather:',
+  //   answers: [
+  //     {answer: 'build something from scratch', program: 'BE'},
+  //     {answer: 'build on top of an existing boilerplate', program: 'FE'},
+  //   ]
+  // },
+  // {
+  //   id: 8,
+  //   question: 'When I receive directions:',
+  //   answers: [
+  //     {answer: 'I can easily conceptualize what to do next', program: 'BE'},
+  //     {answer: 'I would prefer to write things down first', program: 'FE'},
+  //   ]
+  // },
+  // {
+  //   id: 9,
+  //   question: 'I would rather:',
+  //   answers: [
+  //     {answer: 'paint a picture of the mountains', program: 'FE'},
+  //     {answer: 'build the canvas the picture is painted on', program: 'BE'},
+  //   ]
+  // },
+  // {
+  //   id: 10,
+  //   question: 'Making an application visually appealing is important to me',
+  //   answers: [
+  //     {answer: 'false', program: 'BE'},
+  //     {answer: 'true', program: 'FE'},
+  //   ]
+  // },
+  // {
+  //   id: 11,
+  //   question: 'When building an app:',
+  //   answers: [
+  //     {answer: 'I am more interested in how a user will interact with my app', program: 'FE'},
+  //     {answer: 'I am more interested in how my app will interact with the server', program: 'BE'},
+  //   ]
+  // },
+  // {
+  //   id: 12,
+  //   question: 'I would rather:',
+  //   answers: [
+  //     {answer: 'write the logic for when a user clicks a button with JavaScript', program: 'FE'},
+  //     {answer: 'build a game of Battleship from scratch with Ruby', program: 'BE'},
+  //   ]
+  // },
+  // {
+  //   id: 13,
+  //   question: 'If I do a little more research on CSS and what APIs are, I would rather:',
+  //   answers: [
+  //     {answer: 'reformat a server-side request so that frontend is getting data in the correct format', program: 'BE'},
+  //     {answer: 'fix a complex CSS file by going through each property one by one', program: 'FE'},
+  //   ]
+  // },
+  // {
+  //   id: 14,
+  //   question: 'As I research my future career as a software developer, I am more interested in studying these tech stack:',
+  //   answers: [
+  //     {answer: 'JavaScript / React / HTML / CSS', program: 'FE'},
+  //     {answer: 'Ruby / Ruby on Rails / Express / PostgreSQL', program: 'BE'},
+  //   ]
+  // },
+  // {
+  //   id: 15,
+  //   question: 'Without listening to the advice of anyone else, my gut is telling me I\'d be a better fit for:',
+  //   answers: [
+  //     {answer: 'The Frontend program', program: 'FE'},
+  //     {answer: 'The Backend program', program: 'BE'},
+  //   ]
+  // },
+  // {
+  //   id: 16,
+  //   question: 'When building IKEA furniture, I prefer to:',
+  //   answers: [
+  //     { answer: 'wing it - I don\'t need the instructions', program: 'BE' },
+  //     { answer: 'use the diagrams to put everything together', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 17,
+  //   question: 'When I think about my future as a software developer:',
+  //   answers: [
+  //     { answer: 'I want to build the parts of the app the user interacts with', program: 'FE' },
+  //     { answer: 'working primarily with the parts of the app the user doesn't see', program: 'BE' },
+  //   ]
+  // },
+  // {
+  //   id: 18,
+  //   question: 'I have an interest in visual design:',
+  //   answers: [
+  //     { answer: 'true', program: 'FE' },
+  //     { answer: 'false', program: 'BE' },
+  //   ]
+  // },
+  // {
+  //   id: 19,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 20,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 21,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 22,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 23,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 24,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
+  // {
+  //   id: 25,
+  //   question: '',
+  //   answers: [
+  //     { answer: '', program: 'BE' },
+  //     { answer: '', program: 'FE' },
+  //   ]
+  // },
 ]
