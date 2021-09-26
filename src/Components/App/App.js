906a794b37a49f47de9d44d06e22914cb60d1614
Sarah <sarahrudy@gmail.com>
2021-09-26 15:56:52 -0600

    Figure out Route path to Results page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,14 +3,19 @@ import '../App/App.css'
 import NavBar from '../NavBar/NavBar'
 import { Route } from 'react-router-dom'
 import Questions from '../Questions/Questions'
+// import Results from '../Results/Results'
 
 
 const App = () => {
+
+  // const [scoreBE, setScoreBE] = useState(0)
+  // const [scoreFE, setScoreFE] = useState(0)
 
   return (
     <main className="App">
       <NavBar />
       <Route exact path="/" component={ Questions } />
+      {/* <Route exact path="/results" component={ Results } /> */}
     </main>
   )
 }
