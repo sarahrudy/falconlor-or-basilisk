bdcbf9e156a56d87b718e4043bc5eceabc273208
Sarah <sarahrudy@gmail.com>
2021-09-25 21:12:53 -0600

    Replace redundant code with iterator

--- src/Components/QuestionCard/QuestionCard.js
+++ src/Components/QuestionCard/QuestionCard.js
@@ -7,29 +7,22 @@ const QuestionCard = ({ questionIndex, questionProp, characterProp }) => {
   return (
     <article className="card-container">
       <div className="question-and-answers">
-        <p>{ questionIndex }</p>
+        <p>{questionIndex}</p>
         <p>{questionProp.question}</p>
         <form>
-          <div>
-            <label className="answer-a">
-              <input
-                type="radio"
-                value={Object.keys(questionProp)[1]}
-                name="radio-button"
-                className="answer-a" 
-              />{questionProp.answer_be}
-            </label>
-          </div>
-          <div>
-            <label className="answer-b">
-              <input
-                type="radio"
-                value={Object.keys(questionProp)[2]}
-                name="radio-button"
-                className="answer-b"
-              />{questionProp.answer_fe}
-            </label>
-          </div>
+          {questionProp.answers.map(answer => (
+            <div>
+              <label className="answer-a">
+                <input
+                  type="radio"
+                  value={answer.answer}
+                  name="radio-button"
+                  className="answer-a"
+                  // onChange={() => onSelectAnswer(answer.program, questionProp.id)}
+                />{answer.answer}
+              </label>
+            </div>
+          ))}
         </form>
       </div>
       <img className="hogwarts-headshot" src={characterProp.image} alt="Hogwarts headshot"></img>
