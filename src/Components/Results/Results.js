6de57933f2045ba7f0c203017eee9a963de580f9
Sarah <sarahrudy@gmail.com>
2021-09-26 17:35:58 -0600

    Add 'take quiz again' button to Route back to Questions

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
 import '../Results/Results.css' 
 
 const Results = () => {
@@ -8,6 +9,7 @@ const Results = () => {
   return (
     <div className="results-container">
       <h1>{winningHouse === 'FE' ? 'You selected FE!' : 'You selected BE!'}</h1>
+      <Link to='/'><button className="take-quiz-again-button">TAKE QUIZ AGAIN!</button></Link>
     </div>
   )
 }
