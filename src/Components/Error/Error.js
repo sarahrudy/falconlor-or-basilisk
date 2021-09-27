135907641fad7ba7564faccca0172bacc7edf66f
Sarah <sarahrudy@gmail.com>
2021-09-27 12:15:12 -0600

    Add export at bottom of file

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -4,9 +4,11 @@ import voldemort from '../../Images/voldemort.png'
 
 const Error = () => {
   return (
-    <div>
-      <p>Error: Page Not Found</p>
+    <div className='error-container'>
+      <p className='error-message'>Error: Page Not Found</p>
       <img src={ voldemort } alt="page not found" className='page-not-found'></img>
     </div>
   )
 }
+
+export default Error 
